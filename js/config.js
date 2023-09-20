db939008d2add29ffa7c315554f4986887426a3f
Dallali8 <sdallali88@gmail.com>
2023-09-19 20:09:02 -0500

    modify-config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-1_HSGZHoUvD
-        userPoolClientId: '', // e.g. 6rj2rbtdirfld4e95gtbabluno
+        userPoolId: '', // e.g. us-east-1_Wu7P5hyNm
+        userPoolClientId: '', // e.g. 3gd5h5udefefkodo7a5kg5al56
         region: '' // e.g. us-east-1
     },
     api: {
