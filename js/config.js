2c69efb5dc022fdf9bc60e57e02531149c2ff2b9
Dallali8 <sdallali88@gmail.com>
2023-09-19 20:38:41 -0500

    modify-config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_Wu7P5hyNm'
-        userPoolClientId: '3gd5h5udefefkodo7a5kg5al56'
+        userPoolId:'us-east-1_Wu7P5hyNm'
+        userPoolClientId:'3gd5h5udefefkodo7a5kg5al56'
         region: 'us-east-1'
     },
     api: {
