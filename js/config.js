6b2f0f78dde7e1380f593d69f9fb3b1d54540af1
Dallali8 <sdallali88@gmail.com>
2023-09-19 20:26:47 -0500

    modify-config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-1_Wu7P5hyNm
-        userPoolClientId: '', // e.g. 3gd5h5udefefkodo7a5kg5al56
-        region: '' // e.g. us-east-1
+        userPoolId: 'us-east-1_Wu7P5hyNm'
+        userPoolClientId: '3gd5h5udefefkodo7a5kg5al56'
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl:
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
